dd4ee57b216bce8cfc1a4bac66c4b4e1ffbd66d9
BinnyJ <binnyjohn39@gmail.com>
2020-11-05 22:24:49 -0500

    Test case one

--- src/mulebookproject/chaptertwenty.java
+++ src/mulebookproject/chaptertwenty.java
@@ -4,6 +4,7 @@ public class chaptertwenty {
     public static void main(String[] args) {
         System.out.println("Java is fun");
         System.out.println("Hello students");
+        System.out.println("Meron is good woman");
 
     }
 }
